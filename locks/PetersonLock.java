693142fa9257e13ee8eac8bee134e114287aef11
Cable <cableplays1912@gmail.com>
2026-07-27 13:20:31 +0200

    add peterson lock functionality

--- locks/PetersonLock.java
+++ locks/PetersonLock.java
@@ -2,16 +2,29 @@ package locks;
 
 public class PetersonLock extends Lock {
 
+    private static final boolean[] flag = new boolean[2];
+    private static boolean turn = false;
+
+    private final int lockIndex;
+    private final int otherLockIndex;
+
     public PetersonLock(boolean lockId) {
         super(lockId);
+        lockIndex = lockId ? 1 : 0;
+        otherLockIndex = lockId ? 0 : 1;
     }
 
     @Override
     public void lock() {
+        flag[lockIndex] = true;
+        turn = !lockId;
 
+        while (turn != lockId && flag[otherLockIndex]) {
+            // wait
+        }
     }
 
     public void unlock() {
-
+        flag[lockIndex] = false;
     }
 }
